27e8270168ae603a3693ef8715640f68da966d32
G1tHub20 <a7m2cy@gmail.com>
2025-04-29 16:23:56 +0900

    初期化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './App.vue' // ViteでJSに変換されたものをimport
 
-createApp(App).mount('#app')
+const app = createApp(App) // 引数に指定されたApp.vueを元にUIを構築
+app.mount('#app') // index.htmlの指定要素にマウント
